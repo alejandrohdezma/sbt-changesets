fd32176e17c379837e9a614be731e98c5940eddb
Alejandro Hernández <info@alejandrohdezma.com>
2026-07-04 20:22:42 +0200

    Replace JavaConverters with scala.jdk.CollectionConverters
    `scala.collection.JavaConverters` is deprecated since 2.13, which makes it
    a fatal warning on the Scala 3 axis of the upcoming sbt 2.x cross-build.
    `scala.jdk.CollectionConverters` is provided on Scala 2.12 by
    scala-collection-compat, now declared explicitly since we import it
    directly (it was already on sbt's classpath transitively).

--- modules/sbt-changesets/src/main/scala/com/alejandrohdezma/sbt/changesets/Commands.scala
+++ modules/sbt-changesets/src/main/scala/com/alejandrohdezma/sbt/changesets/Commands.scala
@@ -20,7 +20,7 @@ import java.io.File
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 
-import scala.collection.JavaConverters._
+import scala.jdk.CollectionConverters._
 import scala.sys.process._
 import scala.util.Try
 
--- modules/sbt-changesets/src/main/scala/com/alejandrohdezma/sbt/changesets/Settings.scala
+++ modules/sbt-changesets/src/main/scala/com/alejandrohdezma/sbt/changesets/Settings.scala
@@ -19,7 +19,7 @@ package com.alejandrohdezma.sbt.changesets
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 
-import scala.collection.JavaConverters._
+import scala.jdk.CollectionConverters._
 import scala.sys.process._
 import scala.util.Try
 
